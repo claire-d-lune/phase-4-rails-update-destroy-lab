29b4ed3dfbfc0e246dca430481148c794213afdb
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-27 12:56:53 -0700

    Completed all lab deliverables.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -18,9 +19,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = Plant.find(params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 end
